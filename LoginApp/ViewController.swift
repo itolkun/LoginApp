ddf4bc935b49a2934e5a603c6e5fc20476defab6
Aitolkun <totita@list.ru>
2022-09-30 15:07:40 +0600

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Айтолкун Анарбекова on 30/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
