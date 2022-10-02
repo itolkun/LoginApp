ce2877657507f960f433186bac6f63450618d402
Aitolkun <totita@list.ru>
2022-10-03 02:46:29 +0600

    HW5_

--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 
 final class WelcomeViewController: UIViewController {
-
+    
+    
     @IBOutlet var welcomeLabel: UILabel!
     @IBOutlet var hiLabel: UILabel!
     
@@ -20,11 +21,27 @@ final class WelcomeViewController: UIViewController {
         
         welcomeLabel.text = "Welcome, \(userNameText ?? "")!"
         hiLabel.text = "\u{1F44B}"
-
     }
 
     @IBAction func logOut(_ sender: Any) {
         dismiss(animated: true)
     }
+    
+    func setGradientBackground() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.systemPink.cgColor,
+            UIColor.systemBlue.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+                
+        self.view.layer.insertSublayer(gradientLayer, at:0)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setGradientBackground()
+        super.viewWillAppear(animated)
+    }
+
 
 }
