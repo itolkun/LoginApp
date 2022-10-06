504c7d8678dca62a6e772c6d71dc8ec38b4b5215
Aitolkun <totita@list.ru>
2022-10-06 16:25:43 +0600

    HW

--- LoginApp/ViewControllers/InstaViewController.swift
+++ LoginApp/ViewControllers/InstaViewController.swift
@@ -12,24 +12,26 @@ class InstaViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
     
     
-    
-    let images: [UIImage] = [
-        UIImage(named: "dog.jpeg")!,
-        UIImage(named: "paint.jpeg")!,
-        UIImage(named: "undersea.jpeg")!,
-    ]
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var imagesListArray = [UIImage]()
+
+        for _ in 1...4
+        {
+            imagesListArray.append(UIImage(named: "nature.jpeg")!)
+            imagesListArray.append(UIImage(named: "dog.jpeg")!)
+            imagesListArray.append(UIImage(named: "paint.jpeg")!)
+            imagesListArray.append(UIImage(named: "undersea.jpeg")!)
+        }
+
+        self.imageView.animationImages = imagesListArray
+        self.imageView.animationDuration = 10.0
+        self.imageView.startAnimating()
         
         imageView.layer.cornerRadius = 40
         
     }
     
-    
-    @IBAction func nextButtonPressed(_ sender: UIButton) {
-        imageView.image = images[0]
-    }
     
 }
